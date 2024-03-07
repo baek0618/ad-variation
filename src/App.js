a7bbf5e9e8c720921b26305419d92794e6d676d7
khbaek <khbaek@jlkgroup.com>
2024-03-07 19:55:56 +0900

    init

---
+++ src/App.js
@@ -0,0 +1,468 @@
+import React, { useState } from "react";
+import "./App.css";
+import Button from "@mui/material/Button";
+import CountUp from "react-countup";
+import SelectModal from "./components/SelectModal";
+import FadeIn from "react-fade-in";
+import { TypeAnimation } from "react-type-animation";
+import { InView } from "react-intersection-observer";
+
+function App() {
+  const [isOpen, setIsOpen] = useState(false);
+
+  const handleOpen = () => {
+    setIsOpen(true);
+  };
+
+  const handleClose = () => {
+    setIsOpen(false);
+  };
+
+  return (
+    <div className="container">
+      <div className="main1">
+        <div className="header">
+          <img
+            src="/img/icons/adone_WH@2x.png"
+            className="headerLogo"
+            alt="logo"
+          />
+          <div>MENU</div>
+          <div>
+            <span>MY PAGE</span>
+            <Button variant="text" id="langButton">
+              <div
+                style={{
+                  width: "12px",
+                  height: "12px",
+                  borderRadius: "50%",
+                  background: "#515151",
+                  marginRight: "6px",
+                }}
+              />
+              ENG
+            </Button>
+          </div>
+        </div>
+        <div style={{ paddingLeft: "50px" }}>
+          <img
+            className="line"
+            // style={{ marginLeft: "50px" }}
+            src="/img/icons/Line_4@2x.png"
+            alt=""
+          />
+        </div>
+
+        {/* main1 */}
+        <div className="bg1Contents">
+          <div style={{ display: "flex", flexDirection: "row" }}>
+            <div id="bg1_content1">
+              <div style={{ display: "flex", alignItems: "center" }}>
+                <span>Go into</span>
+                <img
+                  src="/img/icons/Arrow_6@2x.png"
+                  alt=""
+                  style={{ width: "16px", marginLeft: "6px" }}
+                />
+              </div>
+            </div>
+            <div id="bg1_content2">
+              <InView>
+                {({ inView, ref, entry }) => (
+                  <div ref={ref}>
+                    {inView && (
+                      <FadeIn transitionDuration={1000}>
+                        <div id="bg1_content2_text1">
+                          Input your
+                          <br />
+                          Photoshop
+                          <br />
+                          AD data source
+                        </div>
+                        <div id="bg1_content2_text2">
+                          The PSD AD file will be varied for the media platform
+                          of your choice
+                        </div>
+                        <div id="bg1_content2_button1">
+                          Input your Photoshop AD data source
+                        </div>
+                        <Button
+                          onClick={handleOpen}
+                          id="startButton"
+                          variant="text"
+                        >
+                          <span>Start</span>
+                          <span>＋</span>
+                        </Button>
+                      </FadeIn>
+                    )}
+                  </div>
+                )}
+              </InView>
+            </div>
+          </div>
+
+          <div id="bg1_content3">
+            Display AD automatic variation service
+            <br />
+            Based on algorithm
+          </div>
+        </div>
+      </div>
+
+      {/* divider */}
+      {/* <img className="line" src="/img/icons/Line_4@2x.png" alt="" /> */}
+
+      {/* main2 */}
+      <div className="main2">
+        <div id="main2_text1">*DA: Display advertise</div>
+        <div
+          style={{
+            display: "flex",
+            justifyContent: "space-between",
+            marginBottom: "50px",
+          }}
+        >
+          <div id="main2_text2">
+            <InView>
+              {({ inView, ref, entry }) => (
+                <div ref={ref}>
+                  {inView && (
+                    <TypeAnimation
+                      sequence={[
+                        // Same substring at the start will only be typed out once, initially
+                        "What is DA* automatic variation service?",
+                        1000, // wait 1s before replacing "Mice" with "Hamsters"
+                      ]}
+                      wrapper="span"
+                      speed={70}
+                      style={{
+                        display: "inline-block",
+                        width: "800px",
+                        height: "140px",
+                      }}
+                      repeat={Infinity}
+                      cursor={false}
+                    />
+                  )}
+                </div>
+              )}
+            </InView>
+          </div>
+          <div id="main2_text3">
+            Advertisers and small businesses can achieve
+            <br />
+            optimal campaign performance at a reasonable cost & time
+          </div>
+        </div>
+        <img className="line" src="/img/icons/Line_4@2x.png" alt="" />
+        <div id="main2_flow">
+          <img
+            src="/img/main2_flow.png"
+            style={{ width: "100%", objectFit: "contain" }}
+            alt=""
+          />
+        </div>
+        <div id="main2_text4">
+          <InView>
+            {({ inView, ref, entry }) => (
+              <div ref={ref}>
+                {inView && (
+                  <FadeIn transitionDuration={1000}>
+                    Big data to train AI-generated
+                    <br />
+                    model-based AD variation algorithms
+                  </FadeIn>
+                )}
+              </div>
+            )}
+          </InView>
+        </div>
+        <img
+          className="line"
+          src="/img/icons/Line_2@2x.png"
+          style={{ marginTop: "60px", marginBottom: "78px" }}
+          alt=""
+        />
+        <div
+          style={{ width: "100%", display: "flex", justifyContent: "center" }}
+        >
+          <img
+            src="/img/main2_algorithm.png"
+            alt=""
+            style={{ width: "75%", objectFit: "contain" }}
+          />
+        </div>
+      </div>
+
+      <div
+        className="main2_needs"
+        style={{
+          width: "100%",
+          display: "flex",
+          justifyContent: "center",
+          marginTop: "140px",
+        }}
+      >
+        <div id="main2_needs_text1">
+          <InView>
+            {({ inView, ref, entry }) => (
+              <div ref={ref}>
+                {inView && (
+                  <FadeIn transitionDuration={1000}>
+                    <div style={{ display: "flex" }}>
+                      <span>
+                        Why is this service
+                        <br />
+                        needed?
+                      </span>
+                      <img
+                        src="/img/icons/service_icon.png"
+                        style={{ height: "130px", marginLeft: "30px" }}
+                        alt=""
+                      />
+                    </div>
+                  </FadeIn>
+                )}
+              </div>
+            )}
+          </InView>
+        </div>
+        <div id="main2_needs_text2">
+          <span>
+            The voice of
+            <br />
+            each user's needs
+          </span>
+        </div>
+        <img
+          src="/img/icons/Line_1@2x.png"
+          alt=""
+          style={{
+            width: "100%",
+            objectFit: "contain",
+            position: "absolute",
+            // background: "red",
+            top: "280px",
+            right: "193px",
+          }}
+        />
+
+        <img
+          src="/img/main2_needs.png"
+          alt=""
+          style={{ width: "100%", objectFit: "contain" }}
+        />
+      </div>
+
+      {/* main3  */}
+      <div className="main3">
+        <div className="main3_contents">
+          <InView>
+            {({ inView, ref, entry }) => (
+              <div ref={ref} style={{ minHeight: "138px" }}>
+                {inView && (
+                  <FadeIn transitionDuration={1000}>
+                    <div id="main3_text1">
+                      Solve AD on all media platforms
+                      <br />
+                      with our service
+                    </div>
+                  </FadeIn>
+                )}
+              </div>
+            )}
+          </InView>
+          <img
+            src="/img/icons/Line_3@2x.png"
+            alt=""
+            style={{ width: "100%", objectFit: "contain" }}
+          />
+          <div className="main3_num_container">
+            <div className="main3_num_wrapper">
+              <div className="main3_num_title">
+                Average
+                <br />
+                production time
+              </div>
+              <div className="main3_num_value">
+                <CountUp start={0} end={36} duration={2.75} enableScrollSpy>
+                  {({ countUpRef, start }) => (
+                    <div style={{ display: "flex", alignItems: "flex-end" }}>
+                      <span
+                        style={{
+                          fontSize: "100px",
+                          display: "flex",
+                          alignItems: "flex-end",
+                          width: "130px",
+                        }}
+                        ref={countUpRef}
+                      />
+                      <div style={{ marginBottom: "10px" }}>S</div>
+                    </div>
+                  )}
+                </CountUp>
+              </div>
+            </div>
+            <div className="main3_num_wrapper">
+              <div className="main3_num_title">money saved</div>
+              <div className="main3_num_value">
+                <CountUp start={0} end={80} duration={2.75} enableScrollSpy>
+                  {({ countUpRef, start }) => (
+                    <div style={{ display: "flex", alignItems: "flex-end" }}>
+                      <span
+                        style={{
+                          fontSize: "6.25rem",
+                          display: "flex",
+                          alignItems: "flex-end",
+                          width: "130px",
+                        }}
+                        ref={countUpRef}
+                      />
+                      <div style={{ marginBottom: "10px" }}>%</div>
+                    </div>
+                  )}
+                </CountUp>
+              </div>
+            </div>
+            <div className="main3_num_wrapper">
+              <div className="main3_num_title">time saved</div>
+              <div className="main3_num_value">
+                <CountUp start={0} end={98} duration={2.75} enableScrollSpy>
+                  {({ countUpRef, start }) => (
+                    <div style={{ display: "flex", alignItems: "flex-end" }}>
+                      <span
+                        style={{
+                          fontSize: "6.25rem",
+                          display: "flex",
+                          alignItems: "flex-end",
+                          width: "130px",
+                        }}
+                        ref={countUpRef}
+                      />
+                      <div style={{ marginBottom: "10px" }}>%</div>
+                    </div>
+                  )}
+                </CountUp>
+              </div>
+            </div>
+          </div>
+          <div className="main3_text_wrapper">
+            <div className="main3_text2">
+              ROAS analysis through A/B test
+              <br />
+              to select the optimal media platform
+            </div>
+            <div className="main3_text2">
+              Minimize double work, designers reduce
+              <br />
+              work time and maximize efficiency
+            </div>
+          </div>
+        </div>
+      </div>
+
+      {/* main4 */}
+      <div className="main4">
+        <div className="main4_text_wrapper">
+          <InView>
+            {({ inView, ref, entry }) => (
+              <div ref={ref}>
+                {inView && (
+                  <FadeIn transitionDuration={1000}>
+                    <div className="main4_text1">
+                      Choose
+                      <br />
+                      the media you
+                      <br />
+                      want to run ads on!
+                    </div>
+                    <div className="main4_text2">
+                      Automatically converts
+                      <br />
+                      numerous sizes and specific specifications
+                    </div>
+                  </FadeIn>
+                )}
+              </div>
+            )}
+          </InView>
+        </div>
+      </div>
+
+      {/* main5 */}
+      <div className="main5">
+        <InView>
+          {({ inView, ref, entry }) => (
+            <div ref={ref}>
+              {inView && (
+                <FadeIn transitionDuration={1000}>
+                  <Button
+                    onClick={handleOpen}
+                    variant="text"
+                    sx={{
+                      textTransform: "none",
+                      color: "#000000",
+                      lineHeight: "1.1",
+                    }}
+                  >
+                    <div
+                      style={{
+                        display: "flex",
+                        flexDirection: "column",
+                        alignItems: "center",
+                      }}
+                    >
+                      <img
+                        src="/img/icons/psd.png"
+                        style={{ width: "280px" }}
+                        alt=""
+                      />
+                      <span
+                        style={{
+                          fontSize: "4.063rem",
+                          fontWeight: "bold",
+                          marginTop: "20px",
+                        }}
+                      >
+                        Start now!
+                      </span>
+                      <span style={{ fontSize: "1.625rem", opacity: 0.6 }}>
+                        Try it for yourself
+                      </span>
+                    </div>
+                  </Button>
+                </FadeIn>
+              )}
+            </div>
+          )}
+        </InView>
+      </div>
+
+      <div className="footer">
+        <div className="footer_text_wrapper">
+          <div className="footer_text1">
+            Corporation owner : Shin Hyangdo
+            <br />
+            Business registration number: 707-67-00536
+            <div style={{ marginTop: "30px" }}>
+              Customer service center: help@ADonecorp.com / +82 10-5049-5476
+            </div>
+          </div>
+          <div className="footer_text2">
+            <img
+              src="/img/icons/adone_WH@2x.png"
+              style={{ width: "120px" }}
+              alt=""
+            />
+            <span>Copyright ⓒ AD.one Corporation All rights reserved.</span>
+          </div>
+        </div>
+      </div>
+
+      <SelectModal open={isOpen} handleClose={handleClose} />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/SelectModal/index.js
@@ -0,0 +1,270 @@
+import React, { useEffect, useState } from "react";
+import Modal from "@mui/material/Modal";
+import styled from "@emotion/styled";
+import Checkbox from "@mui/material/Checkbox";
+import Button from "@mui/material/Button";
+
+const ModalContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+  background-color: white;
+  width: 1000px;
+  height: 700px;
+  border: none;
+  border-radius: 15px;
+  padding: 57px 59px;
+`;
+const Title = styled.div`
+  font-size: 1.875rem;
+  font-weight: bold;
+`;
+const SubTitle = styled.div``;
+
+const Contents = styled.div`
+  display: flex;
+  padding-top: 50px;
+  flex: 1;
+`;
+const UploadFiles = styled.div`
+  display: flex;
+  flex-direction: column;
+`;
+
+const Table = styled.div`
+  display: flex;
+  flex-direction: column;
+  width: 100%;
+`;
+const Row = styled.div`
+  display: flex;
+  width: 100%;
+  padding: 10px 0;
+  align-items: center;
+  border-bottom: 1px solid #d5d5d5;
+  div:nth-child(1) {
+    width: 10%;
+  }
+  div:nth-child(2) {
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    width: 25%;
+  }
+  div:nth-child(3) {
+    width: 25%;
+    font-weight: bold;
+    font-size: 0.875rem;
+  }
+  div:nth-child(4) {
+    width: 40%;
+    display: flex;
+    flex-direction: column;
+    span:nth-child(1) {
+      color: #000000;
+      font-size: 0.875rem;
+    }
+    span:nth-child(2) {
+      color: #666666;
+      font-size: 11px;
+    }
+  }
+`;
+const Layout = styled.div`
+  border: 1px solid #e7e7e7;
+  background: #f5f5f5;
+  border-radius: 4px;
+`;
+const ButtonWrapper = styled.div`
+  display: flex;
+  justify-content: flex-end;
+`;
+const TableView = styled.div`
+  display: flex;
+  flex-direction: column;
+  justify-content: space-between;
+  width: 100%;
+  height: 100%;
+`;
+
+const SelectModal = ({ open, handleClose }) => {
+  const [variantSize, setVariantSize] = useState();
+
+  useEffect(() => {
+    if (!open) {
+      setVariantSize();
+    }
+  }, [open]);
+
+  return (
+    <Modal
+      open={open}
+      onClose={handleClose}
+      aria-labelledby="modal-modal-title"
+      aria-describedby="modal-modal-description"
+      sx={{
+        display: "flex",
+        alignItems: "center",
+        justifyContent: "center",
+      }}
+    >
+      <ModalContainer>
+        <Title>Select variation size</Title>
+        <SubTitle>
+          Choose the right size for your media platforms and get automatic
+          variations
+        </SubTitle>
+        <Contents>
+          <UploadFiles>
+            <img
+              style={{ width: "227px" }}
+              src="/img/icons/Data_11@2x.png"
+              alt=""
+            />
+            <div
+              style={{
+                fontSize: "0.938rem",
+                fontWeight: "bold",
+                marginTop: "12px",
+              }}
+            >
+              Upload file
+            </div>
+            <div
+              style={{
+                padding: "6px 14px",
+                borderRadius: "16px",
+                border: "1px solid #D5D5D5",
+                marginTop: "6px",
+                display: "flex",
+                fontSize: "0.875rem",
+                fontWeight: "bold",
+                justifyContent: "center",
+                alignItems: "center",
+                width: "fit-content",
+              }}
+            >
+              MY PC
+            </div>
+
+            <div
+              style={{
+                fontSize: "0.938rem",
+                fontWeight: "bold",
+                marginTop: "38px",
+              }}
+            >
+              File Name
+            </div>
+          </UploadFiles>
+          <TableView>
+            <Table>
+              <Row>
+                <div>
+                  <span style={{ opacity: 0.3 }}>Check</span>
+                </div>
+                <div style={{ display: "flex" }}>Layout</div>
+                <div style={{ fontWeight: "normal" }}>Size</div>
+                <div>Media Platform</div>
+              </Row>
+              <Row>
+                <div>
+                  <Checkbox
+                    checked={variantSize === 0}
+                    onChange={() => setVariantSize(0)}
+                  />
+                </div>
+                <div>
+                  <Layout style={{ width: "59px", height: "59px" }} />
+                </div>
+                <div>1200 x 1200</div>
+                <div>
+                  <span>1:1 / 1.2:1 / 1:1.2 Ratio</span>
+                  <span>Adison, Facebook, Naver Webtoon ···</span>
+                </div>
+              </Row>
+              <Row>
+                <div>
+                  <Checkbox
+                    checked={variantSize === 1}
+                    onChange={() => setVariantSize(1)}
+                  />
+                </div>
+                <div>
+                  <Layout style={{ width: "59px", height: "32px" }} />
+                </div>
+                <div>1200 x 600</div>
+                <div>
+                  <span>2:1 / 16:9 / 1.91:1 Ratio</span>
+                  <span>Youtube, Naver, Kakao, Toss ···</span>
+                </div>
+              </Row>
+              <Row>
+                <div>
+                  <Checkbox
+                    checked={variantSize === 2}
+                    onChange={() => setVariantSize(2)}
+                  />
+                </div>
+                <div>
+                  <Layout style={{ width: "32px", height: "59px" }} />
+                </div>
+                <div>600 x 1200</div>
+                <div>
+                  <span>1:2 / 9:16 / 1:3.75 Ratio</span>
+                  <span>Cashslide, Snow, Instagram ···</span>
+                </div>
+              </Row>
+              <Row>
+                <div>
+                  <Checkbox
+                    checked={variantSize === 3}
+                    onChange={() => setVariantSize(3)}
+                  />
+                </div>
+                <div>
+                  <Layout style={{ width: "97px", height: "17px" }} />
+                </div>
+                <div>640 x 100</div>
+                <div>
+                  <span>6.4:1 / 8.09:1 / 3.2:1 Ratio</span>
+                  <span>Google, Amazon, InMobi, Cauly ···</span>
+                </div>
+              </Row>
+            </Table>
+            <ButtonWrapper>
+              <Button
+                onClick={handleClose}
+                sx={{
+                  ml: "18px",
+                  background: "#F5F5F5",
+                  color: "#A6A6A6",
+                  borderRadius: "8px",
+                  border: "1px solid #E7E7E7",
+                  padding: "6px 18px",
+                  textTransform: "none",
+                }}
+              >
+                Cancel
+              </Button>
+              <Button
+                variant="contained"
+                sx={{
+                  ml: "18px",
+                  background: "#005CF1",
+                  color: "white",
+                  borderRadius: "8px",
+                  padding: "6px 18px",
+                  textTransform: "none",
+                }}
+              >
+                Variations
+              </Button>
+            </ButtonWrapper>
+          </TableView>
+        </Contents>
+      </ModalContainer>
+    </Modal>
+  );
+};
+
+export default SelectModal;
